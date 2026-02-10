4cbca83ffd6daf6bec06e232a3ec4c8e5984d0dd
Rubel <mr.adcadre41@gmail.com>
2026-02-10 08:04:08 +0600

    json with html added

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ const studentsJSON = JSON.stringify(students);
 console.log(studentsJSON);
 const studentObj = JSON.parse(studentsJSON);
 console.log(studentObj);
+////
+const loadData = () => {
+  console.log("Button clicked!");
+
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error fetching data:", error));
+};
